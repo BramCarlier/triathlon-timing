b1502c8213ec5e55d8eac089b6fb9507c222bd9a
BramCarlier <bram-carlier@hotmail.com>
2026-09-24 14:51:49 +0200

    Wire EN/NL localisation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\ForcePasswordChange;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [HandleInertiaRequests::class, ForcePasswordChange::class]);
+        $middleware->web(append: [SetLocale::class, HandleInertiaRequests::class, ForcePasswordChange::class]);
         $middleware->alias(['role' => RoleMiddleware::class, 'permission' => \App\Http\Middleware\PermissionMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
