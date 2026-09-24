f3af224a32aaf9f5d9eb1e34a5f404fdbb0b55e0
BramCarlier <bram-carlier@hotmail.com>
2026-09-24 14:46:53 +0200

    Add Dutch password messages

---
+++ lang/nl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Je wachtwoord is opnieuw ingesteld.',
+    'sent' => 'We hebben je een link gestuurd om je wachtwoord opnieuw in te stellen.',
+    'throttled' => 'Wacht even voordat je het opnieuw probeert.',
+    'token' => 'Deze link om het wachtwoord opnieuw in te stellen is ongeldig of verlopen.',
+    'user' => 'We kunnen geen gebruiker met dat e-mailadres vinden.',
+];
