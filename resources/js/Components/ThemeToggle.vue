b7953f7a0ecb7eaf8ab024f823dde9cdad4ef01f
BramCarlier <bram-carlier@hotmail.com>
2026-09-24 14:54:15 +0200

    Localise appearance toggle

--- resources/js/Components/ThemeToggle.vue
+++ resources/js/Components/ThemeToggle.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { theme, toggleTheme } from '../theme';
+import { tr } from '../i18n';
+const label = computed(() => theme.value === 'dark' ? tr('Switch to light mode') : tr('Switch to dark mode'));
 </script>
-<template><button type="button" class="btn-icon shrink-0" :aria-label="`Switch to ${theme==='dark'?'light':'dark'} mode`" :title="`Switch to ${theme==='dark'?'light':'dark'} mode`" @click="toggleTheme"><i :class="theme==='dark'?'fa-solid fa-sun':'fa-solid fa-moon'" aria-hidden="true"></i></button></template>
+<template><button type="button" class="btn-icon shrink-0" :aria-label="label" :title="label" @click="toggleTheme"><i :class="theme==='dark'?'fa-solid fa-sun':'fa-solid fa-moon'" aria-hidden="true"></i></button></template>
